55029d29a1a21c3996867f3c94875f111ebb2ac4
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-08-12 15:42:28 +0530

    fix: update API base URL for correct server connection

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api/v1',
+  baseURL: 'http://192.168.137.95:5000/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
